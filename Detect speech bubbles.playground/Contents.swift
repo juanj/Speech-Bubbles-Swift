bfc7b94419edc7b03343324d3930fddcbdc59977
Juan Jose <1958175+juanj@users.noreply.github.com>
2021-01-31 13:09:03 -0500

    Clean images

--- Detect speech bubbles.playground/Contents.swift
+++ Detect speech bubbles.playground/Contents.swift
@@ -82,3 +82,42 @@ let renderedImage = renderer.image { context in
     }
     renderingContext.strokePath()
 }
+
+var bubbles = [UIImage]()
+for contour in contours {
+    let bounds = contour.normalizedPath.boundingBox
+    let scaledRect = CGRect(x: bounds.origin.x * image.size.width, y: bounds.origin.y * image.size.height, width: bounds.size.width * image.size.width, height: bounds.size.height * image.size.height)
+    let renderer = UIGraphicsImageRenderer(size: scaledRect.size)
+    let renderedImage = renderer.image { context in
+        let renderingContext = context.cgContext
+        renderingContext.setFillColor(UIColor.white.cgColor)
+        renderingContext.fill(CGRect(x: 0, y: 0, width: size.width, height: size.height))
+
+        let flipVertical = CGAffineTransform(a: 1, b: 0, c: 0, d: -1, tx: 0, ty: size.height)
+        renderingContext.concatenate(flipVertical)
+        renderingContext.translateBy(x: -scaledRect.origin.x, y: image.size.height - scaledRect.height - scaledRect.origin.y)
+
+        renderingContext.scaleBy(x: size.width, y: size.height)
+        renderingContext.addPath(contour.normalizedPath)
+        renderingContext.clip(using: .evenOdd)
+
+        renderingContext.scaleBy(x: 1 / size.width, y: 1 / size.height)
+        renderingContext.draw(cgImage, in: CGRect(origin: .zero, size: image.size))
+
+        renderingContext.scaleBy(x: size.width, y: size.height)
+        renderingContext.resetClip()
+        renderingContext.addPath(contour.normalizedPath)
+        renderingContext.setLineWidth(5 / size.width)
+        renderingContext.setStrokeColor(UIColor.white.cgColor)
+        renderingContext.strokePath()
+
+    }
+    bubbles.append(renderedImage)
+}
+bubbles[0]
+bubbles[1]
+bubbles[2]
+bubbles[3]
+bubbles[4]
+bubbles[5]
+bubbles[6]
--- README.md
+++ README.md
@@ -11,12 +11,12 @@ The idea with this project is to try to detect speech bubbles on comics/mangas u
 After that, the image should be ready for OCR
 
 ## Progress
-#### Read the image.
+#### Read the Image.
 The image I will be using is a page of the manga [ブラックジャックによろしく](https://densho810.com/free/)
 
 <img src="/images/image1.jpg" width="400">
 
-#### Clean the image
+#### Clean the Image
 For this step I'm applying multiple CIFilters. The speech bubbles should be really obvious after this step.
 
 <img src="/images/image2.png" width="400">
@@ -26,13 +26,18 @@ A `VNDetectContoursRequest` seems to be doing a great job of detecting contours
 
 <img src="/images/image3.jpg" width="400">
 
-#### Filter contours
+#### Filter Contours
 Filter contours that have an area of at least 0.004, and no bigger than 0.4 and that have a proportion of area/perimeter of at least 0.02
 
 <img src="/images/image4.jpg" width="400">
 
+#### Clean the Images
+With the contours identified, it's possible to crop and mask individual bubbles. To help on cleaning the bubble border, the path is stroked with white color.
+
+<img src="/images/bubble1.jpg" width="100"><img src="/images/bubble2.jpg" width="100"><img src="/images/bubble3.jpg" width="100"><img src="/images/bubble4.jpg" width="100"><img src="/images/bubble5.jpg" width="100"><img src="/images/bubble6.jpg" width="100"><img src="/images/bubble7.jpg" width="100">
+
 TODO:
-* After having the most probable speech bubbles identified, it should be easy to split and mask the image to get the individual bubble using Core Graphics.
+* It's possible that some contours inside contours aren't being checked.
 
 
 ## Resources
